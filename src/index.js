142c33a4904a929e4f27aa34a021be2b4b49bbf4
meloshnikov <meloshnikoff@gmail.com>
2023-08-08 00:37:46 +0300

    step1: Adds save File

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-const downloadPage = (url, option) => {
-  console.log('🚀 : run downloadPage : downloadPage:');
-  console.log('🚀 : downloadPage : url:', url);
-  console.log('🚀 : downloadPage : option:', option);
+import path from 'path';
+import { mkdir, writeFile } from 'fs/promises';
+// import { load } from 'cheerio';
+import axios from 'axios';
+
+const getFileName = ({ host, pathname }) => (host + pathname).replace(/[^\d+\w]/g, '-');
+
+const loadHtmlPage = async (dirName, url, fileName) => {
+  const response = await axios.get(url);
+  await writeFile(fileName, response.data);
+};
+
+const downloadPage = async (url, output = process.cwd()) => {
+  const fileName = getFileName(new URL(url));
+  const filePath = path.join(output, `${fileName}.html`);
+
+  return mkdir(output, { recursive: true })
+    .then(() => loadHtmlPage(output, url, filePath))
+    .then(() => console.log('Ok'))
+    .catch((e) => { throw new Error(e); });
 };
 
 export default downloadPage;
