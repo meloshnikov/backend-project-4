bb21edd5761e658dc421bd505ad7e8cca9c463c8
meloshnikov <meloshnikoff@gmail.com>
2023-08-21 02:19:44 +0300

    step3: Adds downloading all local resources from link and script tags

--- src/index.js
+++ src/index.js
@@ -3,27 +3,34 @@ import fs from 'fs/promises';
 import axios from 'axios';
 import {
   getFileName,
-  extractImageUrls,
-  getImagePaths,
+  extractUrlsByTag,
+  getPaths,
   replaceUrls,
   writeFile,
-  downloadImages,
-} from './tools';
+  downloadAssets,
+} from './tools.js';
 
 const loadHtmlPage = async (url, output) => {
-  const fileName = getFileName(new URL(url));
+  const { host, pathname, origin } = new URL(url);
+  const fileName = getFileName({ host, pathname });
   const filesDirName = `${fileName}_files`;
   const htmlPath = path.join(output, `${fileName}.html`);
   const filesPath = path.join(output, filesDirName);
 
+  const downloadedResources = ['img', 'link', 'script'];
+
   try {
     const page = await axios.get(url);
-    const imageUrls = extractImageUrls(page.data);
-    const absoluteImagePaths = getImagePaths(imageUrls, filesPath);
-    const relativeImagePaths = getImagePaths(imageUrls, filesDirName);
-    const htmlWithReplacedImageUrls = replaceUrls(page.data, relativeImagePaths);
-    await writeFile(htmlPath, htmlWithReplacedImageUrls);
-    await downloadImages(imageUrls, absoluteImagePaths);
+    let editedHtml = page.data;
+    downloadedResources.forEach(async (tag) => {
+      const urls = extractUrlsByTag(page.data, origin, tag);
+      if (urls.length) {
+        const paths = getPaths(urls, filesDirName, filesPath);
+        editedHtml = replaceUrls(editedHtml, tag, paths.relative);
+        await downloadAssets(urls, paths.absolute);
+      }
+    });
+    await writeFile(htmlPath, editedHtml);
   } catch (error) {
     console.error(`Failed to download the page: ${error.message}`);
     throw error;
--- src/tools.js
+++ src/tools.js
@@ -3,19 +3,35 @@ import fs from 'fs/promises';
 import { load } from 'cheerio';
 import axios from 'axios';
 
+const attrMapper = {
+  img: 'src',
+  link: 'href',
+  script: 'src',
+};
+
 export const getFileName = ({ host, pathname }) => (host + pathname).replace(/[^\d+\w]/g, '-');
 
-export const getImageName = (imageURL) => imageURL.split('/').pop();
+export const getAssetName = (url) => (url ? url.split('/').pop() : undefined);
 
-export const getImagePaths = (urls, output) => urls.map((url) => {
-  const imageName = getImageName(url);
-  return path.join(output, imageName);
+export const isFile = (url) => !!getAssetName(url).split('.')[1] ?? false;
+
+export const getAssetPaths = (urls, output) => urls.map((url) => {
+  const assetName = getAssetName(url);
+  return path.join(output, assetName);
 });
 
-export const replaceUrls = (html, replacementUrls) => {
+export const getPaths = (urls, relativePath, absolutePath) => ({
+  relative: getAssetPaths(urls, relativePath),
+  absolute: getAssetPaths(urls, absolutePath),
+});
+
+export const replaceUrls = (html, tag, replacementPaths) => {
   const $ = load(html);
-  $('img').each((index, el) => {
-    $(el).attr('src', replacementUrls[index]);
+  const assets = Array.from($(tag));
+  replacementPaths.forEach((replacePath) => {
+    const assetName = getAssetName(replacePath);
+    const elements = assets.filter((el) => getAssetName($(el).attr(attrMapper[tag])) === assetName);
+    elements.forEach((element) => $(element).attr(attrMapper[tag], replacePath));
   });
   return $.html();
 };
@@ -31,15 +47,15 @@ export const downloadFile = async (fileUrl, filePath) => {
   return writeFile(filePath, data);
 };
 
-export const downloadImages = async (urls, absolutePaths) => {
+export const downloadAssets = async (urls, absolutePaths) => {
   await Promise.allSettled(urls.map((url, index) => downloadFile(url, absolutePaths[index])));
 };
 
-export const extractImageUrls = (html) => {
+export const extractUrlsByTag = (html, origin, tag) => {
   const $ = load(html);
-  const images = $('img');
-  return Array.from(images).map((el) => {
-    const src = $(el).attr('src');
-    return new URL(src).toString();
-  });
+  const assets = $(tag);
+  return Array.from(assets)
+    .map((el) => $(el).attr(attrMapper[tag]))
+    .filter((url) => String(url).startsWith('/') || (String(url).includes(origin) && isFile(url)))
+    .map((pathname) => (new URL(pathname, origin)).toString());
 };
